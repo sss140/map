fc81ada26a3ffa0fa255825b659a68c111104ea0
佐藤一成 <kazunarisato0309@gmail.com>
2020-11-15 15:27:32 +0900

    makwAnotherWxView

--- Map/ContentView.swift
+++ Map/ContentView.swift
@@ -8,71 +8,94 @@
 import SwiftUI
 import MapKit
 
+struct Place: Identifiable {
+    var id = UUID()
+    let name: String
+    var latitude: Double
+    var longitude: Double
+    
+    var coordinate: CLLocationCoordinate2D {
+      CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+  }
+
+
+
+
 struct ContentView: View {
+    
+    @State var count:Int = 0
+    @State var position = CLLocationCoordinate2D()
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
     
     @State private var isShownSheet:Bool = false
     @State private var region:MKCoordinateRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 35.655164046, longitude: 139.740663704), span: MKCoordinateSpan(latitudeDelta: 1.0, longitudeDelta: 1.0))
-    @State var offset:CGFloat = 0.0
+    var annotation = MKPointAnnotation()
+    
+    @State var pinPlacce = [Place]()
+    
+    
+    
+    
+    
+    
+    @State var offset:CGFloat = UIScreen.main.bounds.maxY-70
     @ObservedObject var viewModel:ViewModel = ViewModel()
     
     var body: some View {
         ZStack{
             
-            Map(coordinateRegion: $region)
+            Map(coordinateRegion: $region,annotationItems: pinPlacce) { place in
+                MapMarker(coordinate: place.coordinate, tint: .none)}
                 .ignoresSafeArea(.all, edges: .all)
-            Text("\(offset)")
-            
+            VStack{
+                Text("\(self.count)")
+            }
             GeometryReader{geometry in
-                let sheetHeight:CGFloat = geometry.frame(in: .global).height
                 VStack{
                     BottomSheet()
                         .offset(x: 0.0, y: offset)
-                        .gesture(DragGesture().onChanged{ value in
-                            withAnimation{
-                                
-                               // if value.startLocation.y > geometry.frame(in: .global).midX{
-                                offset = value.location.y
-                               
-                                //}
-                                /*
-                                if value.startLocation.y < geometry.frame(in: .global).midX{
-                                    offset = (-sheetHeight + 140) + value.translation.height
-                                }
-                                */
-                                
-                            }
-                        })
+                        .gesture(DragGesture()
+                                    .onChanged{ value in
+                                        withAnimation{
+                                            offset = value.location.y
+                                        }
+                                    }.onEnded({value in
+                                        print(offset)
+                                        withAnimation{
+                                            if offset < UIScreen.main.bounds.maxY/2-30{
+                                                offset = UIScreen.main.bounds.maxY/2-30
+                                            }
+                                            if offset > UIScreen.main.bounds.maxY-70{
+                                                offset = UIScreen.main.bounds.maxY-70
+                                            }
+                                        }
+                                    })
+                        )
                 }
             }
-            
-            //.offset(x: 0.0, y: UIScreen.main.bounds.height-500)
-            /*
-             ZStack{
-             if self.viewModel.isLoading{
-             ProgressView("Loading")
-             }else{
-             Circle()
-             .frame(width: 60.0, height: 60.0, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-             .foregroundColor(self.viewModel.isLoading ? .red : .blue)
-             .opacity(0.5)
-             Text("天気を取得")
-             .font(.caption)
-             }
-             
-             }
-             .onTapGesture(count: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/){
-             
-             
-             self.viewModel.getAPI(lat: region.center.latitude, long: region.center.longitude)
-             }
-             */
             .sheet(isPresented: self.$viewModel.isShownSheet){
                 VStack{
-                    
                     self.viewModel.test()
-                    
                 }
                 
+            }
+        }
+        .onReceive(timer){ input in
+            
+            if (self.position.latitude == self.region.center.latitude) && (self.position.longitude == self.region.center.longitude){
+                self.count += 1
+                if self.count == 3{
+                    let lat:Double = self.region.center.latitude
+                    let lon:Double = self.region.center.longitude
+                    pinPlacce.removeAll()
+                    pinPlacce.append(Place(name: "name", latitude: lat, longitude: lon))
+                    self.viewModel.getAPI(lat: lat, long: lon)
+                }
+            }else{
+                self.position = self.region.center
+                self.count = 0
             }
         }
     }
